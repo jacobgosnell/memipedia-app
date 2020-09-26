2ab9aa7339a1b8b32e632a6d14524e2aabba4a86
Jake Gosnell <jake@hangar31development.com>
2020-09-26 14:28:21 -0700

    added a feedscreen component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import FeedScreen from "./screens/FeedScreen";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
+			<FeedScreen />
 		</View>
 	);
 }
