2f9c7e547fef64620d9db5c2750bdc000d4fbb5d
Jake Gosnell <jake@hangar31development.com>
2020-09-29 21:41:16 -0700

    added navigation feature to feedscreen

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 
-export default () => {
+export default (props) => {
 	return (
 		<View>
 			<Text>Feed Screen</Text>
-			<TouchableOpacity>
+			<TouchableOpacity onPress={() => props.navigation.navigate("Search")}>
 				<Text>Search</Text>
 			</TouchableOpacity>
 		</View>
